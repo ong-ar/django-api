0c22a50eca1f12ecef679ec104feb47fd6b4605a
ong-ar <reclusis@gmail.com>
2018-03-23 15:41:50 +0900

    #1-32

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
 
     # User management
     url(r'^users/', include('django_api.users.urls', namespace='users')),
+    url(r'^images/', include('django_api.images.urls', namespace='images')),
     url(r'^accounts/', include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
