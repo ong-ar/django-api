98797ac56c04a1df3accf2ecab6af591588cf278
ong-ar <reclusis@gmail.com>
2018-03-26 00:36:58 +0900

    #1-40

--- django_api/images/serializers.py
+++ django_api/images/serializers.py
@@ -1,11 +1,8 @@
 from rest_framework import serializers
 from . import models
+from django_api.users import models as user_models
 
-class CommentSerializer(serializers.ModelSerializer):
 
-    class Meta:
-        model = models.Comment
-        fields = '__all__'
 
 class LikeSerializer(serializers.ModelSerializer):
 
@@ -13,10 +10,32 @@ class LikeSerializer(serializers.ModelSerializer):
         model = models.Like
         fields = '__all__'
 
+class FeedUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = user_models.User
+        fields = (
+            'username',
+            'profile_image'
+        )
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    creator = FeedUserSerializer()
+    
+    class Meta:
+        model = models.Comment
+        fields = (
+            'id',
+            'message',
+            'creator'
+        )
+
 class ImageSerializer(serializers.ModelSerializer):
 
     comments = CommentSerializer(many=True)
     likes = LikeSerializer(many=True)
+    creator = FeedUserSerializer()
 
     class Meta:
         model = models.Image
@@ -27,6 +46,9 @@ class ImageSerializer(serializers.ModelSerializer):
             'location',
             'caption',
             'comments',
-            'likes'
+            'likes',
+            'like_count',
+            'creator',
+            
         )
 
