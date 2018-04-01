1128138ab634d1bbf9a0196a35cb7aeb3dca245f
ong-ar <reclusis@gmail.com>
2018-04-01 13:44:23 +0900

    #1-61

--- django_api/images/urls.py
+++ django_api/images/urls.py
@@ -10,19 +10,24 @@ urlpatterns = [
         name='feed'
     ),
     url(
-        regex=r'^(?P<image_id>[0-9]+)/like/',
+        regex=r'^(?P<image_id>[0-9]+)/like/$',
         view=views.LikeImage.as_view(),
         name='like_image'
     ),
     url(
-        regex=r'^(?P<image_id>[0-9]+)/unlike/',
+        regex=r'^(?P<image_id>[0-9]+)/unlike/$',
         view=views.UnLikeImage.as_view(),
         name='unlike_image'
     ),
     url(
-        regex=r'^(?P<image_id>[0-9]+)/comments/',
+        regex=r'^(?P<image_id>[0-9]+)/comments/$',
         view=views.CommentOnImage.as_view(),
         name='comment_image'
+    ),
+    url(
+        regex=r'^(?P<image_id>[0-9]+)/comments/(?P<comment_id>[0-9]+)/$',
+        view=views.ModerateComment.as_view(),
+        name='moderate_comment'
     ),
     url(
         regex=r'^comments/(?P<comment_id>[0-9]+)/$',
