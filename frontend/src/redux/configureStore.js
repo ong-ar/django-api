99b4147395baab8092b39847a7e4ab62e84130cd
ong-ar <reclusis@gmail.com>
2018-04-07 22:57:57 +0900

    #3-6 add logger

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -2,11 +2,18 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import users from "redux/modules/users";
 
+const env = process.env.NODE_ENV;
+
 const middlewares = [thunk];
 
 const reducer = combineReducers({
   users
 });
+
+if (env === "development") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
 
 let store = initialState =>
   createStore(reducer, applyMiddleware(...middlewares));
