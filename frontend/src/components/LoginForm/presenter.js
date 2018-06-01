c010885a7a68a33b0b527a4b72dd26710d684308
ong-ar <reclusis@gmail.com>
2018-06-01 16:33:59 +0900

    #3-34

--- frontend/src/components/LoginForm/presenter.js
+++ frontend/src/components/LoginForm/presenter.js
@@ -5,16 +5,22 @@ import formStyles from "shared/formStyles.scss";
 
 const LoginForm = props => (
   <div className={formStyles.formComponent}>
-    <form className={formStyles.form}>
+    <form className={formStyles.form} onSubmit={props.handleSubmit}>
       <input
         type="text"
         placeholder="Username"
         className={formStyles.textInput}
+        value={props.usernameValue}
+        onChange={props.handleInputChange}
+        name="username"
       />
       <input
         type="password"
         placeholder="Password"
         className={formStyles.textInput}
+        value={props.passwordValue}
+        onChange={props.handleInputChange}
+        name="password"
       />
       <input type="submit" value="Log in" className={formStyles.button} />
     </form>
@@ -26,6 +32,13 @@ const LoginForm = props => (
     <span className={formStyles.forgotLink}>Forgot password?</span>
   </div>
 );
+
+LoginForm.PropTypes = {
+  usernameValue: PropTypes.string.isRequired,
+  passwordValue: PropTypes.string.isRequired,
+  handleInputChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired
+};
 
 LoginForm.contextTypes = {
   t: PropTypes.func.isRequired
